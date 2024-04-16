350b3c4e69ca6f002b63680d58d401ef7a2c27d5
Xth-Dragon <164742509+Xth-Dragon@users.noreply.github.com>
2024-04-16 16:51:41 +0200

    Version 1.1
    + ScrollTop en appuyant sur le toggle hamburger.

--- JS/hamburger.js
+++ JS/hamburger.js
@@ -4,5 +4,8 @@ const navLinks = document.querySelector(".nav-links")
 menuHamburger.addEventListener('click',()=>{
 navLinks.classList.toggle('mobile-menu')
 document.body.classList.toggle('lock');
-
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
 })
