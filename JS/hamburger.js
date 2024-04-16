79e690163e0bc2253fbc3122aa4b4af2f61b59cd
Xth-Dragon <164742509+Xth-Dragon@users.noreply.github.com>
2024-04-16 17:11:48 +0200

    Version 1.2
    Correctif pour le Scroll.

--- JS/hamburger.js
+++ JS/hamburger.js
@@ -3,9 +3,6 @@ const navLinks = document.querySelector(".nav-links")
 
 menuHamburger.addEventListener('click',()=>{
 navLinks.classList.toggle('mobile-menu')
-document.body.classList.toggle('lock');
-function topFunction() {
-  document.body.scrollTop = 0; 
-  document.documentElement.scrollTop = 0; 
-}
+document.body.classList.toggle('lock');document.body.scrollTop = 0; document.documentElement.scrollTop = 0; 
+
 })
